fb2e363ebaf36979e76e76de362a807e05278aec
Sebastian Otaegui <feniix@gmail.com>
2014-06-24 12:25:47 -0500

    Do log activity and verify AWS creds properly

--- puppet/modules/local_files/files/backup-to-s3.sh
+++ puppet/modules/local_files/files/backup-to-s3.sh
@@ -3,8 +3,8 @@
 SRC_DIR="/srv/nexus/sonatype-work"
 DATE=$(date +"%Y%m%d")
 
-if [ ! -z ${AWS_ACCESS_KEY_ID} ] && [ ! -z ${AWS_SECRET_ACCESS_KEY} ]; then
+if [ -z ${AWS_ACCESS_KEY_ID} ] || [ -z ${AWS_SECRET_ACCESS_KEY} ]; then
   echo "Make sure the environment variables AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY are defined for user: $(whoami)"
   exit 1
 fi
-aws s3 sync --quiet --delete $SRC_DIR "s3://spantree-nexus-backup/$DATE"  --exclude "*central/*"  --exclude "*central-m1/*"  --exclude "*apache-snapshots/*"  --exclude "*codehaus-snapshots/*"  --exclude "*public/*"  --exclude "*thirdparty/*" --exclude "*/indexer*" --exclude "*/felix-cache*"
+aws s3 sync --delete $SRC_DIR "s3://spantree-nexus-backup/$DATE"  --exclude "*central/*"  --exclude "*central-m1/*"  --exclude "*apache-snapshots/*"  --exclude "*codehaus-snapshots/*"  --exclude "*public/*"  --exclude "*thirdparty/*" --exclude "*/indexer*" --exclude "*/felix-cache*"
